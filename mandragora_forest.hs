80412372de3e25c0e13c42d23578da8077d007d6
Yuriy Netesov <973520+dehun@users.noreply.github.com>
2016-10-05 23:14:41 +0200

    solve candies

--- mandragora_forest.hs
+++ mandragora_forest.hs
@@ -3,25 +3,29 @@ import Data.Either
 import Debug.Trace    
 import Control.Applicative
 import Data.List
-import Control.Monad    
+import Control.Monad
+import qualified Data.Vector as V    
 
+sss :: V.Vector Int -> Int -> Int -> Int -> Int
+sss xs idx right_sum best_so_far 
+    | idx == V.length xs = best_so_far
+    | otherwise = 
+    let current = (right_sum) * (idx + 1 )
+        el = (V.!) xs idx 
+    in if current <= best_so_far
+       then best_so_far
+       else sss xs (idx + 1) (right_sum - el) current
+    
 
-best_journey :: [Int] -> Int
+best_journey :: V.Vector Int -> Int
 best_journey ms =
-    let res = foldM (\(left_sum, right_sum, best_so_far) m ->
-                             if left_sum * right_sum < best_so_far
-                             then Left best_so_far
-                             else traceShowId $ Right (left_sum + 1, right_sum - m, left_sum * right_sum)
-          ) (1, sum ms, 0) ms
-    in case res of
-         Left r -> r
-         Right (_, _, r) -> r
-
-
+    sss ms 0 (V.sum ms) 0   
+    
+    
 main = do
   t <- read <$> getLine 
   mapM_ (\_ -> do
            n <- read <$> getLine
            ms <-  map read <$> take n <$> words <$> getLine
-           putStrLn $ show $ best_journey $ sort ms
+           putStrLn $ show $ best_journey $ V.fromList $ sort ms
         ) [1..t]
