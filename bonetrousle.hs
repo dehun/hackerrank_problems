4684bdaa35821f541cd54d488dd64e21100dcaa9
Yuriy Netesov <973520+dehun@users.noreply.github.com>
2016-10-07 09:09:26 +0200

    a bit faster bonetrousle solution

--- bonetrousle.hs
+++ bonetrousle.hs
@@ -6,20 +6,24 @@ import Debug.Trace
     
 
 
-solve :: [Int] -> Int -> Int -> Maybe [Int]
-solve xs n b =
---    trace ("solving " ++ show xs ++ "; n=" ++ show n ++ "; b=" ++ show b) $ $ traceShowId $
-          solve_ xs n b
+solve :: [Int] -> Int -> Int -> Int -> Maybe [Int]
+solve xs k n b =
+--    trace ("solving " ++ show xs ++ "; k=" ++ show k ++ "; n=" ++ show n ++ "; b=" ++ show b) $ --traceShowId $
+          solve_ xs k n b
 
-solve_ :: [Int] -> Int -> Int -> Maybe [Int]
-solve_ [] n b = if b == 0 && n == 0 then Just [] else Nothing
-solve_ (x:xs) n b
+solve_ :: [Int] -> Int -> Int -> Int -> Maybe [Int]
+solve_ [] k n b = if b == 0 && n == 0 then Just [] else Nothing
+solve_ (x:xs) k n b
+       | k < b = Nothing
+       | (k) * (x + x + k) `div` 2 < n = Nothing
        | (b <= 0) = Nothing
        | (n <= 0) = Nothing
-       | (n == x) && (b == 1) = trace "found" $ Just [x]
+       | (n == x) && (b == 1) =
+--           trace "found" $
+                 Just [x]
        | otherwise =
-    let cases = [((:) x <$> (solve xs (n - x) (b - 1)))
-                , solve xs n b] :: [Maybe [Int]]
+    let cases = [((:) x <$> (solve xs (k - 1) (n - x) (b - 1)))
+                , solve xs (k - 1) n b] :: [Maybe [Int]]
         first_success = join (find isJust cases) :: Maybe [Int]
     in first_success
       
@@ -28,6 +32,6 @@ main = do
   t <- read <$> getLine
   mapM_ (\_ -> do
            [n, k, b] <-  map read <$> words <$> getLine
-           let s = (solve [1..k] n b) :: Maybe [Int]
+           let s = (solve [1..k] k n b) :: Maybe [Int]
            putStrLn $ unwords $ map show (maybe [-1] id s)
         ) [1..t]
