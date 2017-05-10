c7d54c039601f3a9db2ef0d955ac0151f9a456d1
Yuriy Netesov <yuriy.netesov@gmail.com>
2017-05-10 09:40:47 +0200

    password cracker

---
+++ password_cracker.hs
@@ -0,0 +1,74 @@
+import Data.List
+import qualified Data.Map.Strict as M
+import Data.Functor
+import Control.Monad
+import Data.Maybe    
+import Debug.Trace    
+
+data Trie = TNode { t_c :: Char, t_subs :: M.Map Char [Trie] }
+          | TRoot { t_subs :: M.Map Char [Trie] }
+          | TLeaf { t_c :: Char} deriving (Show, Eq)
+
+emptyTrie = TRoot M.empty
+emptySubtrie c = TNode c M.empty
+
+buildTrie :: [String] -> Trie
+buildTrie xs = foldl (\trie x -> insertInTrie trie x) emptyTrie $ (nub $ sortBy (\s1 s2 -> compare (length s2) (length s1)) xs)
+
+insertInTrie :: Trie -> String -> Trie
+insertInTrie (TRoot subs) (c:[]) =
+    case M.lookup c subs of
+      Nothing -> TRoot (M.insert c [TLeaf c] subs)
+      Just subtries -> TRoot (M.insert c (TLeaf c:subtries) subs)
+insertInTrie (TRoot subs) (c:cs) =
+    case M.lookup c subs of
+      Nothing ->
+          let nt = insertInTrie (emptySubtrie c) cs
+          in TRoot (M.insert c [nt] subs)
+      Just subtries -> TRoot (M.insert c (map (\t -> insertInTrie t cs) subtries) subs)
+insertInTrie (TNode nc subs) (c:[]) =
+    case M.lookup c subs of
+      Nothing -> TNode nc (M.insert c [TLeaf c] subs)
+      Just subtries -> TNode nc (M.insert c (TLeaf c:subtries) subs)
+insertInTrie (TNode nc subs) (c:cs) =
+    case M.lookup c subs of 
+      Nothing -> let nt = insertInTrie (emptySubtrie c) cs
+                 in TNode nc (M.insert c [nt] subs)
+      Just subtries -> TNode nc (M.insert c (map (\t -> insertInTrie t cs) subtries) subs)
+insertInTrie (TLeaf c)  _ = TLeaf c
+
+subqueryTrie :: Trie -> String -> String -> [(String, String)]
+subqueryTrie (TLeaf c) cs res = [(reverse res, cs)]
+subqueryTrie _ [] res = []
+subqueryTrie subtrie (c:cs) res =
+    let matchingSubs = case M.lookup c (t_subs subtrie) of
+                         Just subs -> subs
+                         Nothing -> []
+    in concatMap (\sub -> subqueryTrie sub cs (t_c sub : res)) matchingSubs
+
+queryTrie :: Trie -> String -> [(String, String)]
+queryTrie trie la = subqueryTrie trie la ""
+
+
+subsolve :: Trie -> String -> Maybe [String]
+subsolve trie ""  = Just []
+subsolve trie la  =
+    let parses = (reverse $ queryTrie trie la) 
+        subseqs  =  map (\(parse, rest) -> (:) parse <$> subsolve trie rest) parses
+    in join $ find isJust subseqs
+
+solve :: [String] -> String -> Maybe [String]
+solve ps la =
+    let trie = buildTrie ps
+    in subsolve trie la 
+
+main = do
+  n <- read <$> getLine :: IO Int
+  qs <- mapM (\_ -> do
+                nw <- read <$> getLine
+                ps <- take nw <$> words <$> getLine
+                la <- getLine
+                return (ps, la)) [1..n]
+  mapM (\(ps, la) -> case solve ps la of
+                       Nothing -> putStrLn "WRONG PASSWORD"
+                       Just psseq -> putStrLn $ concat $ intersperse " " psseq) qs
